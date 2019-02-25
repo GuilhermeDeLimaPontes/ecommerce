77b5aa750a866ca24ea0dfa6860e6f72e4810775
GuilhermeDeLimaPontes <guilhermepontes6@gmail.com>
2019-02-25 03:07:52 -0300

    tela de login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -8,6 +8,8 @@ class Page
 	private $tpl;
 	private $options = [];
 	private $defaults = [
+		"header"=>true,
+		"footer"=>true,
 		"data"=>[]
 	];
 
@@ -26,7 +28,9 @@ class Page
 
 		$this->setData($this->options["data"]);
 
-		$this->tpl->draw("header");
+		if($this->options["header"] === true){
+			$this->tpl->draw("header");
+		}
 	}	
 
 	private function setData($data = array())
@@ -45,8 +49,12 @@ class Page
 
 	public function __destruct()
 	{
-		$this->tpl->draw("footer");
+		if($this->options["footer"] === true)
+		{
+			$this->tpl->draw("footer");
+		}
 	}
+
 }
 
 ?>
