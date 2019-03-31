d654e5f7e41b557b616fb77122bdfd9260526055
GuilhermeDeLimaPontes <guilhermepontes6@gmail.com>
2019-03-31 16:24:22 -0300

    Termino do projeto

--- vendor/hcodebr/php-classes/src/Model/OrderStatus.php
+++ vendor/hcodebr/php-classes/src/Model/OrderStatus.php
@@ -13,7 +13,14 @@ Class OrderStatus extends Model
 	const PAGO = 3;
 	const ENTREGUE = 4;
 
+	public static function listAll()
+	{
+		$sql = new Sql();
 
+		return $sql->select("SELECT * FROM tb_ordersstatus ORDER BY desstatus");
+	}
 }
+
+
 
 ?>
